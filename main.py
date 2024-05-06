f45c2ceb7de5b40e1fa4c9faa405373491f581d4
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-05-05 22:54:47 -0700

    Edited query params

--- main.py
+++ main.py
@@ -30,17 +30,17 @@ async def postGoals(goal: Goal):
 
 class Meal(BaseModel):
     name: str
-    calroies: int
+    calories: int
 
-@app.post("/meal")
-async def postMeal(meal: Meal):
+@app.post("/meal/{customer_id}")
+async def postMeal(meal: Meal, customer_id: int):
     with db.engine.begin() as connection:
         sql = "INSERT INTO meal (name, calories, customer_id, ingredient_id)\
             VALUES (:name, :calories, :customer_id, :ingredient_id)"
         
         result = connection.execute(sqlalchemy.text(sql), 
                                     [{"name": meal.name, "calories":meal.calories, 
-                                      "customer_id": 1, "daily_calories" : 1}])
+                                      "customer_id": 1, "daily_calories" : 1, "ingredient_id": 1}])
     return "OK"
 
 
@@ -62,7 +62,7 @@ async def getDailyCalories(customer_id: int):
 @app.get("/meal/{customer_id}/day")
 async def getAllMeals(customer_id: int):
     with db.engine.begin() as connection:
-        sql = "SELECT name, calories, time, type, ingredient_id FROM meal WHERE custoemr_id = :customer_id"
+        sql = "SELECT name, calories, time, type, ingredient_id FROM meal WHERE customer_id = :customer_id"
         meals = connection.execute(sqlalchemy.text(sql), [{"customer_id": customer_id}]).fetchall()
 
         meal_list = []
